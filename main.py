67878579aedd23b57b2e385f8886e4b15ae462ed
incognito <sylsterior@gmail.com>
2025-08-10 18:58:40 +0100

    firsst commit

---
+++ main.py
@@ -0,0 +1,13 @@
+i=1
+while i<10:
+    print(i)
+    i+=1
+j=int(input("enter a number"))
+k=0
+sum=0
+while k<=j:
+           sum+=k
+           k+=1
+print("The sum of the numbers from 0 to " , j , "is", sum)
+
+    
